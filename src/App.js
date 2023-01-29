c7283dd6fbe4c5d548fe3c3cea5709cf64371ecd
AnnaVans <119419794+AnnaVans@users.noreply.github.com>
2023-01-29 11:03:00 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather app
-      <a href="https://github.com/AnnaVans/react-app-weather" target="_blank">
-        Git repository
-      </a>
+      <h1>Weather app</h1>
+      <p>
+        Coded by Anna Vansacova, open-sourced on{" "}
+        <a href="https://github.com/AnnaVans/react-app-weather" target="_blank">
+          Github
+        </a>
+      </p>
     </div>
   );
 }
