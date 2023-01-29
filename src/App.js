9b80de1da326bce806cfec91c1042b043ffb454a
AnnaVans <119419794+AnnaVans@users.noreply.github.com>
2023-01-29 11:00:26 +0100

    added git link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <a href="https://github.com/AnnaVans/react-app-weather" target="_blank">
+        Git repository
+      </a>
+    </div>
+  );
 }
 
 export default App;
